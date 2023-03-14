d1be84896794cfe73a71eeda1f30b4e705a2211a
Anthony K <kilospade@gmail.com>
2023-03-14 04:14:12 -0500

    fix localhost not working on firefox

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   server: {
+    host: "127.0.0.1",
     port: 3000,
   },
   build: {
-    target: 'esnext',
+    target: "esnext",
   },
 });
